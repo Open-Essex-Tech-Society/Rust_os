c3b44eee41e35a3d5203638b7135d1de48a4b2cf
TatsuyaM2667 <miuranosuketatsuya06@gmail.com>
2026-05-12 19:27:32 +0900

    コマンド実装

--- target/x86_64-unknown-none/debug/deps/rust_os-6801e2d996c52076.d
+++ target/x86_64-unknown-none/debug/deps/rust_os-6801e2d996c52076.d
@@ -1,6 +1,6 @@
-/home/tatsuya/Rust_os/target/x86_64-unknown-none/debug/deps/rust_os-6801e2d996c52076.d: src/main.rs src/vga_buffer.rs src/serial.rs src/gdt.rs src/interrupts.rs src/memory.rs src/allocator.rs src/shell.rs
+/home/tatsuya/Rust_os/target/x86_64-unknown-none/debug/deps/rust_os-6801e2d996c52076.d: src/main.rs src/vga_buffer.rs src/serial.rs src/gdt.rs src/interrupts.rs src/memory.rs src/allocator.rs src/shell.rs src/pci.rs
 
-/home/tatsuya/Rust_os/target/x86_64-unknown-none/debug/deps/librust_os-6801e2d996c52076.rmeta: src/main.rs src/vga_buffer.rs src/serial.rs src/gdt.rs src/interrupts.rs src/memory.rs src/allocator.rs src/shell.rs
+/home/tatsuya/Rust_os/target/x86_64-unknown-none/debug/deps/librust_os-6801e2d996c52076.rmeta: src/main.rs src/vga_buffer.rs src/serial.rs src/gdt.rs src/interrupts.rs src/memory.rs src/allocator.rs src/shell.rs src/pci.rs
 
 src/main.rs:
 src/vga_buffer.rs:
@@ -10,3 +10,4 @@ src/interrupts.rs:
 src/memory.rs:
 src/allocator.rs:
 src/shell.rs:
+src/pci.rs:
--- target/x86_64-unknown-none/debug/deps/rust_os-e56ba460d114c95c.d
+++ target/x86_64-unknown-none/debug/deps/rust_os-e56ba460d114c95c.d
@@ -1,6 +1,6 @@
-/home/tatsuya/Rust_os/target/x86_64-unknown-none/debug/deps/rust_os-e56ba460d114c95c.d: src/main.rs src/vga_buffer.rs src/serial.rs src/gdt.rs src/interrupts.rs src/memory.rs src/allocator.rs src/shell.rs
+/home/tatsuya/Rust_os/target/x86_64-unknown-none/debug/deps/rust_os-e56ba460d114c95c.d: src/main.rs src/vga_buffer.rs src/serial.rs src/gdt.rs src/interrupts.rs src/memory.rs src/allocator.rs src/shell.rs src/pci.rs
 
-/home/tatsuya/Rust_os/target/x86_64-unknown-none/debug/deps/librust_os-e56ba460d114c95c.rmeta: src/main.rs src/vga_buffer.rs src/serial.rs src/gdt.rs src/interrupts.rs src/memory.rs src/allocator.rs src/shell.rs
+/home/tatsuya/Rust_os/target/x86_64-unknown-none/debug/deps/librust_os-e56ba460d114c95c.rmeta: src/main.rs src/vga_buffer.rs src/serial.rs src/gdt.rs src/interrupts.rs src/memory.rs src/allocator.rs src/shell.rs src/pci.rs
 
 src/main.rs:
 src/vga_buffer.rs:
@@ -10,3 +10,4 @@ src/interrupts.rs:
 src/memory.rs:
 src/allocator.rs:
 src/shell.rs:
+src/pci.rs:
